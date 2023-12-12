e9f3599aebce7ef03049b00beeaa241d386fffff
YOUR-USERNAME <dan.stacy@alltech.com>
2023-12-12 14:15:15 -0500

    Fixed Next config to allow images.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
-
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "creative.alltech.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.ctfassets.net",
+      },
+      {
+        protocol: "https",
+        hostname: "www.alltech.com",
+      },
+    ],
+    formats: ["image/avif", "image/webp"],
+		loader: "default",
+  },
+};
