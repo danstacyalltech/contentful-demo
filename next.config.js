2a7defb44a0ee94b0d00ec4885757cdf85651833
YOUR-USERNAME <dan.stacy@alltech.com>
2023-12-12 08:39:05 -0500

    Updeate Next.config to fix issue with pictures

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  images: {
-    loader: 'custom',
-    formats: ['image/avif', 'image/webp'],
-  },
-}
+const nextConfig = {}
+
+module.exports = nextConfig
+
