57071fbb5d0e5c4eda6064cee47916fde13fc92f
YOUR-USERNAME <dan.stacy@alltech.com>
2023-12-12 14:20:02 -0500

    Added LinkHeader component

---
+++ components/LinkHeader.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const LinkHeader = () => {
+	return (
+		<header className="py-3 bg-black text-white font-bold flex justify-around px-20">
+      <p>Newsroom</p>
+      <p>Contacts and Resources</p>
+      <p>Press Releases</p>
+      <p>Blog</p>
+      <p>Podcast</p>
+      <p>In The News</p>
+      <p>Feeding Times</p>
+    </header>
+	)
+}
+
+export default LinkHeader
