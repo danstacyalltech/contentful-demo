7e690602edecfcc85a577809203160225d08b465
YOUR-USERNAME <dan.stacy@alltech.com>
2023-12-12 14:19:18 -0500

    Created SearchHeader component

---
+++ components/SearchHeader.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Searchheader = () => {
+  return (
+    <header className="text-5xl text-white font-bold bg-terracotta pl-24 py-8 ">
+      The Alltech Blog
+    </header>
+  );
+};
+
+export default Searchheader;
